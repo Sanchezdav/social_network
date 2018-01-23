f889b00c58206efdb2bd9334127d664f026c96e5
Unknown <sanchez.dav90@gmail.com>
2018-01-23 08:10:53 -0600

    Login Facebook errors

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
+  post "/custom_sign_up", to: "users/omniauth_callbacks#custom_sign_up"
+
   root 'main#home'
 end
